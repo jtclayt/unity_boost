6dbeff9c7641b619dd2267b105216f3124b6d98c
Justin Clayton <jt.clayton92@yahoo.com>
2020-08-25 08:08:08 -0700

    Implement simple object tagging

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float RCSThrust = 175f;
+    [SerializeField] float VerticalThrust = 3f;
     Rigidbody rigidBody;
     AudioSource boosterAudio;
+
+    const string FRIENDLY_TAG = "Friendly";
+    const string FINISH_TAG = "Finish";
 
     // Start is called before the first frame update
     void Start()
@@ -17,17 +22,15 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput()
+    private void Thrust()
     {
-        Vector3 thrust = 2 * Vector3.up;
-        Vector3 rotation = 50 * Vector3.forward * Time.deltaTime;
-
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(thrust);
+            rigidBody.AddRelativeForce(Vector3.up * VerticalThrust);
             if (!boosterAudio.isPlaying)
             {
                 boosterAudio.Play();
@@ -37,14 +40,40 @@ public class Rocket : MonoBehaviour
         {
             boosterAudio.Stop();
         }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag(FRIENDLY_TAG))
+        {
+            print("alive");
+        }
+        else if (collision.gameObject.CompareTag(FINISH_TAG))
+        {
+            print("win");
+        }
+        else
+        {
+            print("dead");
+        }
+    }
+
+    private void Rotate()
+    {
+        Vector3 rotationThisFrame = RCSThrust * Time.deltaTime * Vector3.forward;
+
+        // Take control of rotation
+        rigidBody.freezeRotation = true;
 
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Rotate(rotation);
+            transform.Rotate(rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Rotate(-rotation);
+            transform.Rotate(-rotationThisFrame);
         }
+
+        // Release control of rotation to physics engine
+        rigidBody.freezeRotation = false;
     }
 }
